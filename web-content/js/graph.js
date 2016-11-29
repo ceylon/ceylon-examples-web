5c66e567a4b2920a7aec085dc9e9c3975a035098
Tako Schotanus <tako@ceylon-lang.org>
2016-11-29 21:28:00 +0100

    Updated to Ceylon 1.3.1

--- web-content/js/graph.js
+++ web-content/js/graph.js
@@ -11,7 +11,7 @@ require.config({
             'ceylon/language/1.2.0/ceylon.language-1.2.0': langMod,
             'ceylon/language/1.2.1/ceylon.language-1.2.1': langMod,
             'ceylon/language/1.2.2/ceylon.language-1.2.2': langMod,
-            'ceylon/language/1.3.0/ceylon.language-1.3.0': langMod
+            'ceylon/language/1.3.1/ceylon.language-1.3.1': langMod
 			/* Don't put the latest version here, only previous versions */
         }
     }
