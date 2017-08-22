3d90e16886d690eb7ace53f16e02b6861e82fda7
Tako Schotanus <tako@ceylon-lang.org>
2017-08-22 21:06:22 +0200

    Fixed some refs that were pointing to newer version 1.3.1 instead of 1.3.0

--- web-content/js/graph.js
+++ web-content/js/graph.js
@@ -1,5 +1,5 @@
 
-var ceylonVersion = "1.3.1";
+var ceylonVersion = "1.3.0";
 
 var langMod = 'ceylon/language/' + ceylonVersion + '/ceylon.language-' + ceylonVersion;
 
@@ -10,14 +10,13 @@ require.config({
         '*': {
             'ceylon/language/1.2.0/ceylon.language-1.2.0': langMod,
             'ceylon/language/1.2.1/ceylon.language-1.2.1': langMod,
-            'ceylon/language/1.2.2/ceylon.language-1.2.2': langMod,
-            'ceylon/language/1.3.0/ceylon.language-1.3.0': langMod
+            'ceylon/language/1.2.2/ceylon.language-1.2.2': langMod
 			/* Don't put the latest version here, only previous versions */
         }
     }
 });
 
-require(['com/redhat/demo/graph/1.3.1/com.redhat.demo.graph-1.3.1'], function(graph) {
+require(['com/redhat/demo/graph/1.3.0/com.redhat.demo.graph-1.3.0'], function(graph) {
     graph.run();
 });
 
